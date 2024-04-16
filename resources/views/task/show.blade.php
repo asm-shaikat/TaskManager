28c85b9365a6579e35250b532bdb1b8a7e164400
asm-shaikat <asmshaikat1952@gmail.com>
2024-04-17 00:03:41 +0600

    feat: show select dropdown for Category

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -7,16 +7,20 @@
             <h3 class="text-2xl font-semibold inline-flex">{{ $task->title }}</h3>
 
 
-            <div class="mb-4">
-                <select id="status-select" class="form-select bg-green-700 text-white badge" onchange="updateTaskStatus(this.value)">
-                    <option value="todo" {{ $task->status === 'todo' ? 'selected' : '' }}>To Do</option>
-                    <option value="backlog" {{ $task->status === 'backlog' ? 'selected' : '' }}>Backlog</option>
-                    <option value="in_progress" {{ $task->status === 'in_progress' ? 'selected' : '' }}>In Progress</option>
-                    <option value="in_review" {{ $task->status === 'in_review' ? 'selected' : '' }}>In Review</option>
-                    <option value="done" {{ $task->status === 'done' ? 'selected' : '' }}>Done</option>
-                    <option value="achieved" {{ $task->status === 'achieved' ? 'selected' : '' }}>Achieved</option>
-                </select>
+            <div class="dropdown inline-block relative">
+                <span class="cursor-pointer badge bg-green-600 text-white inline" id="status-name" onclick="toggleStatusDropdown()">{{ ucfirst($task->status) }}</span>
+                <div id="status-dropdown" class="hidden absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+                    <select id="status-select" class="form-select bg-slate-600 text-white border" onchange="selectStatus(this.value)">
+                        <option value="todo" {{ $task->status === 'todo' ? 'selected' : '' }}>To Do</option>
+                        <option value="backlog" {{ $task->status === 'backlog' ? 'selected' : '' }}>Backlog</option>
+                        <option value="in_progress" {{ $task->status === 'in_progress' ? 'selected' : '' }}>In Progress</option>
+                        <option value="in_review" {{ $task->status === 'in_review' ? 'selected' : '' }}>In Review</option>
+                        <option value="done" {{ $task->status === 'done' ? 'selected' : '' }}>Done</option>
+                        <option value="achieved" {{ $task->status === 'achieved' ? 'selected' : '' }}>Achieved</option>
+                    </select>
+                </div>
             </div>
+
         </div>
 
         <div class="mb-4">
@@ -31,7 +35,7 @@
 
         <div class="dropdown inline-block relative">
             <h4 class="text-lg font-semibold inline">Assigned to:</h4>
-            <span class="cursor-pointer bg-cyan-700 text-white badge inline" id="user-name" onclick="toggleDropdown()">{{ $task->user->name }}</span>
+            <span class="cursor-pointer badge bg-slate-600 text-white inline" id="user-name" onclick="toggleDropdown()">{{ $task->user->name }}</span>
             <div id="user-dropdown" class="hidden absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
                 <select id="user-select" class="form-select bg-white border" onchange="selectUser(this.value)">
                     @foreach($users as $user)
@@ -41,29 +45,37 @@
             </div>
         </div>
 
-        <div class="mb-4">
-            <div class="mb-4 flex items-center">
+        <div class="mb-2">
+            <div class="flex items-center">
                 <h4 class="text-lg font-semibold mr-2">Priority:</h4>
-                <span class="cursor-pointer bg-cyan-700 text-white badge inline" id="task-priority" onclick="toggleDropdownPriority()">{{ $task->priority }}</span>
-                <div id="priority-dropdown" class="hidden absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+                @if($task->priority=='low')
+                <span class="cursor-pointer badge inline" id="task-priority" onclick="toggleDropdownPriority()">{{ $task->priority }}</span>
+                @elseif($task->priority=='medium')
+                <span class="cursor-pointer bg-yellow-600 text-white badge inline" id="task-priority" onclick="toggleDropdownPriority()">{{ $task->priority }}</span>
+                @else
+                <span class="cursor-pointer bg-red-600 text-white badge inline" id="task-priority" onclick="toggleDropdownPriority()">{{ $task->priority }}</span>
+                @endif
+                <div id="priority-dropdown" class="hidden absolute ml-40 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
                     <select id="priority-select" class="form-select bg-white border" onchange="updateTaskPriority(this.value)">
-                        <option value="low" class="priority-low" {{ $task->priority === 'low' ? 'selected' : '' }}>Low</option>
-                        <option value="medium" class="priority-medium" {{ $task->priority === 'medium' ? 'selected' : '' }}>Medium</option>
-                        <option value="high" class="priority-high" {{ $task->priority === 'high' ? 'selected' : '' }}>High</option>
+                        <option value="low" {{ $task->priority === 'low' ? 'selected' : '' }}>Low</option>
+                        <option value="medium" {{ $task->priority === 'medium' ? 'selected' : '' }}>Medium</option>
+                        <option value="high" {{ $task->priority === 'high' ? 'selected' : '' }}>High</option>
                     </select>
                 </div>
             </div>
 
 
-            <div class="mb-4 flex items-center">
-                <h4 class="text-lg font-semibold mr-2">Category:</h4>
-                <div class="flex items-center">
-                    <select id="category-select" class="form-select bg-white border" onchange="updateTaskCategory(this.value)">
+            <div class="dropdown inline-block relative">
+                <h4 class="text-lg font-semibold inline">Category:</h4>
+                <span class="cursor-pointer badge bg-slate-600 text-white inline" id="category-name" onclick="toggleCategoryDropdown()">{{ ucfirst($task->category) }}</span>
+                <div id="category-dropdown" class="hidden absolute mt-2 ml-20 w-48 bg-white rounded-md shadow-lg z-10">
+                    <select id="category-select" class="form-select bg-white border" onchange="selectCategory(this.value)">
                         <option value="work" {{ $task->category === 'work' ? 'selected' : '' }}>Work</option>
                         <option value="personal" {{ $task->category === 'personal' ? 'selected' : '' }}>Personal</option>
                     </select>
                 </div>
             </div>
+
 
             <div class="mb-4 relative w-1/3">
                 <label for="due_date" class="block text-sm font-medium text-gray-600">Due Date</label>
@@ -142,9 +154,14 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 <script>
-    // Update status
-    function updateTaskStatus(status) {
-        var taskId = '{{$task-> id}}';
+    // Show status dropdown on click
+    document.getElementById('status-name').addEventListener('click', function() {
+        document.getElementById('status-dropdown').classList.toggle('hidden');
+    });
+
+    // Update task status
+    function selectStatus(status) {
+        var taskId = '{{ $task->id }}';
         $.ajax({
             type: 'PUT',
             url: '/task/' + taskId,
@@ -153,14 +170,25 @@
                 status: status
             },
             success: function(response) {
-                // Update UI or show success message
+                document.getElementById('status-name').innerText = status;
+                toggleStatusDropdown();
             },
             error: function(xhr, status, error) {
-                // Handle error
+                console.error(error);
             }
         });
     }
+
+    // Hide status dropdown on click outside
+    document.addEventListener('click', function(event) {
+        var dropdown = document.getElementById('status-dropdown');
+        // Check if the clicked element is not part of the dropdown
+        if (!dropdown.contains(event.target) && event.target !== document.getElementById('status-name')) {
+            dropdown.classList.add('hidden'); // Hide the dropdown
+        }
+    });
     // End of Update Status 
+
     // Update priority status
     // Show dropdown on click
     document.getElementById('task-priority').addEventListener('click', function() {
@@ -178,9 +206,8 @@
                 priority: priority
             },
             success: function(response) {
-                // Handle success response or UI update
-                document.getElementById('priority-select').value = priority; // Update the selected option
-                document.getElementById('priority-dropdown').classList.add('hidden'); // Hide the dropdown
+                document.getElementById('task-priority').innerText = priority;
+                toggleDropdown();
             },
             error: function(xhr, status, error) {
                 // Handle error
@@ -188,14 +215,7 @@
         });
     }
 
-    // Add an event listener to detect clicks outside of the priority dropdown
-    document.addEventListener('click', function(event) {
-        var dropdown = document.getElementById('priority-dropdown');
-        // Check if the clicked element is not part of the dropdown or select element
-        if (!dropdown.contains(event.target) && event.target !== document.getElementById('priority-select')) {
-            dropdown.classList.add('hidden'); // Hide the dropdown
-        }
-    });
+
 
     // Add an event listener to detect clicks outside of the dropdown
     document.addEventListener('click', function(event) {
@@ -205,9 +225,14 @@
         }
     });
 
-    // End of Update Status
     // Start Update Category
-    function updateTaskCategory(category) {
+    // Show category dropdown on click
+    document.getElementById('category-name').addEventListener('click', function() {
+        document.getElementById('category-dropdown').classList.toggle('hidden');
+    });
+
+    // Update task category
+    function selectCategory(category) {
         var taskId = '{{ $task->id }}';
         $.ajax({
             type: 'PUT',
@@ -217,13 +242,24 @@
                 category: category
             },
             success: function(response) {
-                // Handle success response or UI update
+                document.getElementById('category-name').innerText = category;
+                toggleCategoryDropdown();
             },
             error: function(xhr, status, error) {
-                // Handle error
+                console.error(error);
             }
         });
     }
+
+    // Hide category dropdown on click outside
+    document.addEventListener('click', function(event) {
+        var dropdown = document.getElementById('category-dropdown');
+        // Check if the clicked element is not part of the dropdown
+        if (!dropdown.contains(event.target) && event.target !== document.getElementById('category-name')) {
+            dropdown.classList.add('hidden'); // Hide the dropdown
+        }
+    });
+
 
 
     // End  Update Category
